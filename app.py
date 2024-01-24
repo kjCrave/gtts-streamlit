21a8a1fb7ef87d3667a22c574c5843e495d55721
kjCrave <125541605+kjCrave@users.noreply.github.com>
2024-01-24 22:03:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,21 +3,24 @@ from gtts import gTTS
 import os
 
 # Function to convert text to speech
-def text_to_speech(text):
+def text_to_speech(text, filename):
     tts = gTTS(text=text, lang='en')
-    tts.save("output.mp3")
+    tts.save(f"{filename}.mp3")
 
 # Streamlit UI components
 st.title('Text to Speech Converter')
+
+filename = st.text_input("Enter file name")
+
 text = st.text_area("Enter Text", height=300)
 if st.button('Convert'):
-    text_to_speech(text)
+    text_to_speech(text, filename)
     st.success("Conversion Successful!")
 
-    with open("output.mp3", "rb") as file:
+    with open(f"{filename}.mp3", "rb") as file:
         st.download_button(
             label="Download Audio File",
             data=file,
-            file_name="output.mp3",
+            file_name=f"{filename}.mp3",
             mime="audio/mpeg"
         )
