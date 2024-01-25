0c898be5baca8637f9853e2d0b0e92f400f4c0b5
kjCrave <125541605+kjCrave@users.noreply.github.com>
2024-01-25 17:52:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,26 @@
 import streamlit as st
-import pyttsx3
-import tempfile
+from gtts import gTTS
 import os
 
-# Function to convert text to speech using pyttsx3
-def text_to_speech_pyttsx3(text, filename, speech_rate):
-    engine = pyttsx3.init()
-    engine.setProperty('rate', speech_rate)
-    engine.save_to_file(text, filename)
-    engine.runAndWait()
+# Function to convert text to speech
+def text_to_speech(text, filename):
+    tts = gTTS(text=text, lang='en')
+    tts.save(f"{filename}.mp3")
 
 # Streamlit UI components
-st.title('Text to Speech Converter with Pyttsx3')
+st.title('Text to Speech Converter')
 
-text_input = st.text_area("Enter Text", height=300)
-speech_rate = st.selectbox("Select Speech Rate", ["Normal", "Slow"])
+filename = st.text_input("Enter file name")
 
+text = st.text_area("Enter Text", height=300)
 if st.button('Convert'):
-    # Create a temporary directory to save the audio file
-    temp_dir = tempfile.mkdtemp()
-    
-    # Define the file path for the pyttsx3 audio
-    pyttsx3_filename = os.path.join(temp_dir, "output_audio.wav")
-    
-    # Convert text to speech using pyttsx3
-    speech_rate = 200 if speech_rate == "Normal" else 100  # Adjust speech rate as needed
-    text_to_speech_pyttsx3(text_input, pyttsx3_filename, speech_rate)
-    
-    # Download button for the pyttsx3 audio
-    st.markdown("## Download Audio")
-    with open(pyttsx3_filename, "rb") as pyttsx3_file:
+    text_to_speech(text, filename)
+    st.success("Conversion Successful!")
+
+    with open(f"{filename}.mp3", "rb") as file:
         st.download_button(
-            label="Download Audio",
-            data=pyttsx3_file,
-            file_name="output_audio.wav",
-            mime="audio/wav"
+            label="Download Audio File",
+            data=file,
+            file_name=f"{filename}.mp3",
+            mime="audio/mpeg"
         )
